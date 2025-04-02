48e9fc327281d2059e5f9f06d2f143ba8ac45f1c
David Brotman <david@123websitebiz.com>
2025-04-02 14:31:30 -0700

    Troubleshooting activationLink

--- utilities/sendActivationEmail.js
+++ utilities/sendActivationEmail.js
@@ -4,6 +4,9 @@ const sendActivationEmail = async (email, accessCode) => {
   //Construct the activation link
   const activationLink = `${process.env.FRONTEND_URL}/activate-user?email=${email}&code=${accessCode}`;
 
+  //check if activation link is being generated
+  console.log("Activation Link:", activationLink);
+
   // Configure the transporter with your email service
   const transporter = nodemailer.createTransport({
     host: "smtp.mailersend.net",
