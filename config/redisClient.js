1236fe02a2ba4dd218288cee1837c883161760bc
David Brotman <david@123websitebiz.com>
2025-04-04 19:51:35 -0700

    Adding redis service

---
+++ config/redisClient.js
@@ -0,0 +1,24 @@
+const redis = require("redis");
+
+const redisClient = redis.createClient({
+  url: "redis://red-cvo7gaumcj7s73fus710:6379", // Replace with your Redis URL
+});
+
+redisClient.on("error", (error) => {
+  console.error("Redis Client Error:", error);
+});
+
+redisClient.connect().then(() => {
+  console.log("Redis Connected Successfully!");
+
+  // PING test
+  redisClient.ping((err, res) => {
+    if (err) {
+      console.error("Redis PING Error:", err);
+    } else {
+      console.log("Redis Connection Test:", res); // Should print "PONG"
+    }
+  });
+});
+
+module.exports = redisClient;
