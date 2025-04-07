086a6d416396d102625c3842b921ddc97c009cc1
David Brotman <david@123websitebiz.com>
2025-04-06 23:22:34 -0700

    update redis configuration

--- config/redisClient.js
+++ config/redisClient.js
@@ -1,4 +1,4 @@
-cconst redis = require("redis");
+const redis = require("redis");
 
 const redisClient = redis.createClient({
   url: "rediss://default:b972afc35018708f98b6f198711d7987@master.app-keydb-addon--f7dc6rb57l8c.addon.code.run:6379", // Replace with Northflank's REDIS_MASTER_URL
