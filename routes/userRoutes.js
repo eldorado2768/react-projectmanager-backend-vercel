7d556b1e4185a55d95d2646085c246b9acfe7c70
David Brotman <david@123websitebiz.com>
2025-04-15 23:53:49 -0700

    update exports

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { setPassword } from "../controllers/userController.js";
 import * as userController from "../controllers/userController.js";
-import { protect } from "../middleware/protect.js";
+import protect from "../middleware/protect.js";
 
 const router = express.Router();
 
