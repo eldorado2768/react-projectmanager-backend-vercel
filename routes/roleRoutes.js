f90c9eeb073a801753da4dc69f3375bded27e57b
David Brotman <david@123websitebiz.com>
2025-04-15 23:56:35 -0700

    update imports

--- routes/roleRoutes.js
+++ routes/roleRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import roleController from "../controllers/roleController.js";
-import { protect } from "../middleware/protect.js";
+import  protect from "../middleware/protect.js";
 
 const router = express.Router();
 
