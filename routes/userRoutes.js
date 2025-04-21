2830d745bb10a747ff2c11d10ea847c1e8fe28a3
David Brotman <david@123websitebiz.com>
2025-04-20 19:02:38 -0700

    update userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import * as userController from "../controllers/userController.js";
 import protect from "../middleware/protect.js";
+import checkSessionActivity from "../middleware/sessionMiddleware.js";
 
 const router = express.Router();
 
