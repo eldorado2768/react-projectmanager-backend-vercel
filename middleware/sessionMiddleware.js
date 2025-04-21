e2a0bcf29a0026bdf9911ace947f105c7e27173f
David Brotman <david@123websitebiz.com>
2025-04-20 19:27:57 -0700

    troubleshoot sessionmiddleware

--- middleware/sessionMiddleware.js
+++ middleware/sessionMiddleware.js
@@ -1,6 +1,6 @@
 const checkSessionActivity = async (req, res, next) => {
   const sessionId = req.headers["x-session-id"]; // Expect sessionId in headers
-  console.log("checkSessionActivity is running for request:", req.url);
+  console.log("sessionId at top of checkSessionActivity:", sessionId);
 
   if (!sessionId) {
     return res.status(401).json({ message: "Session ID required." });
