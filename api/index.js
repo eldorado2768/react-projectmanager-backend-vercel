2e6e3d5839901b19bdf4f8ce678e5a900f1f4f23
David Brotman <david@123websitebiz.com>
2025-04-28 18:53:21 -0700

    adapt express for Vercel

---
+++ api/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import userRoutes from "../routes/userRoutes.js";
+
+const app = express();
+
+// Middleware
+app.use(express.json()); // Parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+
+// Mount routes
+app.use("/api/users", userRoutes);
+
+export default app;
