7759939b463878c9eeaf42dc1a2dd53aef4665b9
David Brotman <david@123websitebiz.com>
2025-04-30 11:30:44 -0700

    reintroduce userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,28 @@
 import express from "express";
+import * as userController from "../controllers/userController.js";
 import protect from "../middleware/protect.js";
+import checkSessionActivity from "../middleware/sessionMiddleware.js";
 
 const router = express.Router();
 
-router.post("/login-user", protect, async (req, res) => {
-  res.status(200).send("Login route works with protect middleware!");
-});
+const asyncHandler = (fn) => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(next);
+};
+
+router.post("/activate-user", asyncHandler(userController.activateUser));
+router.post("/register-user", asyncHandler(userController.registerUser));
+router.post("/login-user", asyncHandler(userController.loginUser));
+router.post("/forgot-password", asyncHandler(userController.forgotPassword));
+router.post("/reset-password", asyncHandler(userController.resetPassword));
+router.post("/set-password", asyncHandler(userController.setPassword));
+router.post("/refresh-token", asyncHandler(userController.refreshToken));
+router.post("/logout-user", asyncHandler(userController.logoutUser));
+router.get(
+  "/profile",
+  protect,
+  checkSessionActivity,
+  asyncHandler(userController.getUserProfile)
+);
+router.put("/profile", protect, asyncHandler(userController.updateUserProfile));
 
 export default router;
