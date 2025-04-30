84adbe98d819be88332b18bafbcf23eaa1e05a60
David Brotman <david@123websitebiz.com>
2025-04-29 20:25:27 -0700

    update route in index.js

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 // Mount routes
 app.use("/api/users", userRoutes);
 
-app.get("/test", (req, res) => {
+app.get("/api/test", (req, res) => {
   console.log("Test route hit!");
   res.status(200).send("Test route works!");
 });
