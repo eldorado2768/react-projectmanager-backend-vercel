be274ebd1a8a01a25c09ca7d5c733dec21e68d7a
David Brotman <david@123websitebiz.com>
2025-05-01 14:50:22 -0700

    removed test files from routes

--- api/test.js
+++
@@ -1,6 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2N2YwMmNkMjc1ODE4NWFkYmY0YmMwNmYiLCJyb2xlIjoic3VwZXJhZG1pbiIsImlhdCI6MTc0NTk5MzI4MCwiZXhwIjoxNzQ1OTk2ODgwfQ.TtoC0ayNf7jDRnRlildZbBKhCDSPHIuSW7ZJk0bGc7w";
-const decoded = jwt.verify(token, "WW]LlR[fG]YZaoptZL[cW238EH05FJln-GXbE@");
-console.log("Decoded token:", decoded);
