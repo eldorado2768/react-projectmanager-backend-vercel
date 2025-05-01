6ed6f027863d5993d3fcef897cc34af0beeb063d
David Brotman <david@123websitebiz.com>
2025-05-01 15:29:46 -0700

    troubleshoot login-user.js

--- api/userRoutes/login-user.js
+++ api/userRoutes/login-user.js
@@ -1,8 +1,9 @@
 import db from "../../server.js"; //database connection
-import * as userController from "../../controllers/userController.js"; 
+import * as userController from "../../controllers/userController.js";
 import asyncHandler from "../../middleware/asyncHandler.js";
 
 export default async function handler(req, res) {
+  console.log("Incoming login request:", req.body);
   try {
     if (req.method === "POST") {
       return asyncHandler(userController.loginUser)(req, res);
@@ -12,6 +13,8 @@ export default async function handler(req, res) {
     return res.status(405).json({ message: "Method not allowed" });
   } catch (error) {
     console.error("Route handler error:", error.message);
-    return res.status(500).json({ message: "Server error", error: error.message });
+    return res
+      .status(500)
+      .json({ message: "Server error", error: error.message });
   }
 }
